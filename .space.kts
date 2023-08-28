5836340ebdf9acfe8f56f6e315dd3cd5ea7c993c
Felipe Christian Lautenschlager <felipelauten@gmail.com>
2023-08-28 02:10:03 -0300

    Using CI example script

--- .space.kts
+++ .space.kts
@@ -4,15 +4,12 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
-}
-
-job("Build WebAnalyser") {
-	container(displayName = "Builder", image = "maven:latest")
-    shellScript {
-        content = """
-            mvn clean install
-        """
+job("Build and run tests") {
+    container(displayName = "Run mvn install", image = "maven:latest") {
+        shellScript {
+            content = """
+	            mvn clean install
+            """
+        }
     }
 }
